c5b9dd5b9ba35aed3fb8446bee2af30118d6f88c
darbyduggan <darbyduggan13@gmail.com>
2026-09-17 16:07:08 -0400

    Update hero image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import heroImg from './assets/bg.jpg'
 import catImg from './assets/Cat.jpeg'
 import chickImg from './assets/Chick.jpeg'
 import cowImg from './assets/Cow.jpeg'
@@ -18,8 +16,6 @@ function App() {
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
 
         <div>
